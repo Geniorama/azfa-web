2abd12820bd1b563eaa88c561ebf2e9da8812eaf
angelpublicista <angelpublicista.1@gmail.com>
2025-07-09 10:35:11 -0500

    feat: add heading page component

--- src/app/normativa-legal/page.tsx
+++ src/app/normativa-legal/page.tsx
@@ -1,6 +1,10 @@
+import HeadingPage from "@/components/HeadingPage";
+import Cover from "@/assets/img/cover-normativa-legal.webp";
 
 export default function NormativaLegal() {
   return (
-    <div>Normativa Legal</div>
+    <div>
+      <HeadingPage title="Normativa Legal" description="Consulte la normativa legal de cada país" image={Cover.src} />
+    </div>
   )
 }
---
+++ src/components/HeadingPage.tsx
@@ -0,0 +1,33 @@
+interface HeadingPageProps {
+  title: string;
+  description: string;
+  image?: string;
+  textAlign?: "left" | "center" | "right";
+  className?: string;
+}
+
+export default function HeadingPage({
+  title,
+  description,
+  image,
+  textAlign = "center",
+  className,
+}: HeadingPageProps) {
+  return (
+    <div
+      style={{ backgroundImage: `url(${image || ""})` }}
+      className={`bg-primary bg-cover bg-center bg-no-repeat py-16 px-12 relative ${
+        textAlign === "left"
+          ? "text-left"
+          : textAlign === "center"
+          ? "text-center"
+          : "text-right"
+      } ${className}`}
+    >
+      <h1 className="text-background-1 text-h3 lg:text-6xl font-normal">
+        {title}
+      </h1>
+      <p className="text-details text-h6 lg:text-lg mt-4">{description}</p>
+    </div>
+  );
+}
