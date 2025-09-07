7533bbb4a3a1f7d97d138e3e764adfd635714a90
angelpublicista <angelpublicista.1@gmail.com>
2025-09-06 23:04:09 -0500

    feat: slider testimonials

--- src/components/CardEvent.tsx
+++ src/components/CardEvent.tsx
@@ -7,15 +7,16 @@ interface CardEventProps {
     category: string;
     date?: string;
     location?: string;
+    hotel?: string;
     button: {
         label: string;
         onClick: () => void;
     }
 }
 
-export default function CardEvent({ image, title, category, date, location, button }: CardEventProps) {
+export default function CardEvent({ image, title, category, date, location, hotel, button }: CardEventProps) {
   return (
-    <div className="flex border border-background-2 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl overflow-hidden">
+    <div className="flex flex-col lg:flex-row border border-background-2 rounded-tr-2xl rounded-bl-2xl rounded-br-2xl overflow-hidden">
         <div className="w-full lg:w-2/5">
             <img src={image} alt={title} className='w-full h-full object-cover' />
         </div>
@@ -25,15 +26,25 @@ export default function CardEvent({ image, title, category, date, location, butt
             <span className="text-body2 mb-1 inline-block">{category}</span>
             <h3 className="text-h6">{title}</h3>
             <hr className="my-4 border-background-2" />
-            <div className="flex flex-row gap-2 justify-between">
-                <div className="flex flex-row gap-2 items-center">
-                    <GoCalendar />
-                    <span className="text-body2 inline-block">{date}</span>
-                </div>
-                <div className="flex flex-row gap-2 items-center">
-                    <GoHome />
-                    <span className="text-body2 inline-block">{location}</span>
-                </div>
+            <div className="flex flex-row gap-y-2 justify-between flex-wrap">
+                {date && (
+                    <div className="flex flex-row gap-2 items-center w-full lg:w-1/2">
+                        <GoCalendar />
+                        <span className="text-body2 inline-block">{date}</span>
+                    </div>
+                )}
+                {location && (
+                    <div className="flex flex-row gap-2 items-center w-full lg:w-1/2">
+                        <GoHome />
+                        <span className="text-body2 inline-block">{location}</span>
+                    </div>
+                )}
+                {hotel && (
+                    <div className="flex flex-row gap-2 items-center w-full lg:w-1/2">
+                        <GoHome />
+                        <span className="text-body2 inline-block">{hotel}</span>
+                    </div>
+                )}
             </div>
             <hr className="my-4 border-background-2" />
             <button className='flex items-center gap-2 justify-between w-full cursor-pointer text-button hover:opacity-50 transition-all duration-300'>
