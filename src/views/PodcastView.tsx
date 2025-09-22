6753e141f1f1a0e8fc3bc64473a6ad334cbe7e71
angelpublicista <angelpublicista.1@gmail.com>
2025-09-22 11:10:00 -0500

    fix: podcast error

--- src/views/PodcastView.tsx
+++ src/views/PodcastView.tsx
@@ -24,7 +24,7 @@ export default function PodcastView() {
 
                     {/* Embed podcast spotify */}
                     <div className="flex justify-center mt-10">
-                        <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/episode/7GUWEzjS504WOTNpI0YRoU/video?utm_source=generator" width="624" height="351" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                        <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/episode/7GUWEzjS504WOTNpI0YRoU/video?utm_source=generator" width="624" height="351"  allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                     </div>
                 </div>
             </div>
