faf846b21e0b79f11de0c986e27662c30d26f5a1
angelpublicista <angelpublicista.1@gmail.com>
2025-09-22 11:04:01 -0500

    fix: embed podcast

--- src/views/PodcastView.tsx
+++ src/views/PodcastView.tsx
@@ -23,6 +23,9 @@ export default function PodcastView() {
                     </TitleDecorative>
 
                     {/* Embed podcast spotify */}
+                    <div className="flex justify-center mt-10">
+                        <iframe data-testid="embed-iframe" style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/episode/7GUWEzjS504WOTNpI0YRoU/video?utm_source=generator" width="624" height="351" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    </div>
                 </div>
             </div>
         </section>
