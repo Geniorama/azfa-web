7248ea36f78fc4f09d90d85baf6e7831809d961f
angelpublicista <angelpublicista.1@gmail.com>
2025-10-10 03:41:26 -0500

    fix: super position slider arrows home

--- src/components/SlideSingleHome.tsx
+++ src/components/SlideSingleHome.tsx
@@ -15,7 +15,7 @@ interface SlideSingleHomeProps {
 export default function SlideSingleHome({ caption, title, description, button }: SlideSingleHomeProps) {
 
   return (
-    <div className="container mx-auto px-4 relative z-20">
+    <div className="container mx-auto px-4 relative z-10">
       {/* overlay */}
       <div className="space-y-4 w-full lg:w-1/2 text-center lg:text-left text-white">
         <h5 className="text-caption text-details">{caption}</h5>
