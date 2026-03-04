4c87b25e767827639a55719736ab44f74f7de77a
angelpublicista <angelpublicista.1@gmail.com>
2026-03-04 10:54:31 -0500

    fix: GTM Layout useEffect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/types/componentsType";
 import Script from "next/script";
 import { Metadata } from "next";
+import GoogleTagManager from "@/components/GoogleTagManager";
 
 const interTight = Inter_Tight({
   variable: "--font-inter-tight",
@@ -36,56 +37,35 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const globalSettings = await fetchAPI(
-    "/api/global-setting?populate[0]=header&populate[1]=header.logo&populate[2]=header.topButtons&populate[3]=header.mainMenu&populate[4]=header.availableLanguages&populate[5]=socialMedia&populate[6]=socialMedia.icon&populate[7]=socialMedia.icon.customImage&populate[8]=footer&populate[9]=footer.contactInfo&populate[10]=footer.contactInfo.logo&populate[11]=footer.footerLinksColumns&populate[12]=footer.footerLinksColumns.links&populate[13]=footer.copyright&populate[14]=footer.copyright.developedByLink&populate[15]=footer.copyright.legalLinks&populate[16]=contactInfo&populate[17]=contactInfo.phoneIcon&populate[18]=contactInfo.phoneIcon.customImage&populate[19]=contactInfo.emailIcon&populate[20]=contactInfo.emailIcon.customImage&populate[21]=contactInfo.addressIcon&populate[22]=contactInfo.addressIcon.customImage&populate[23]=header.mainMenu.icon&populate[24]=header.mainMenu.icon.customImage&populate[25]=header.mainMenu.submenu"
-  );
-  const header = globalSettings.data.header as HeaderTypeData;
-  const socialMedia = globalSettings.data.socialMedia as SocialMediaItemType[];
-  const footer = globalSettings.data.footer as FooterType;
-  const contactInfoGlobal = globalSettings.data.contactInfo as ContactInfoType;
+  let header: HeaderTypeData;
+  let footerWithContactInfo: FooterType & { contactInfoGlobal: ContactInfoType; socialMedia: SocialMediaItemType[] };
 
-  const footerWithContactInfo = {
-    ...footer,
-    contactInfoGlobal,
-    socialMedia,
-  };
-
-  console.log("footerWithContactInfo", footerWithContactInfo);
-
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  try {
+    const globalSettings = await fetchAPI(
+      "/api/global-setting?populate[0]=header&populate[1]=header.logo&populate[2]=header.topButtons&populate[3]=header.mainMenu&populate[4]=header.availableLanguages&populate[5]=socialMedia&populate[6]=socialMedia.icon&populate[7]=socialMedia.icon.customImage&populate[8]=footer&populate[9]=footer.contactInfo&populate[10]=footer.contactInfo.logo&populate[11]=footer.footerLinksColumns&populate[12]=footer.footerLinksColumns.links&populate[13]=footer.copyright&populate[14]=footer.copyright.developedByLink&populate[15]=footer.copyright.legalLinks&populate[16]=contactInfo&populate[17]=contactInfo.phoneIcon&populate[18]=contactInfo.phoneIcon.customImage&populate[19]=contactInfo.emailIcon&populate[20]=contactInfo.emailIcon.customImage&populate[21]=contactInfo.addressIcon&populate[22]=contactInfo.addressIcon.customImage&populate[23]=header.mainMenu.icon&populate[24]=header.mainMenu.icon.customImage&populate[25]=header.mainMenu.submenu"
+    );
+    const footer = globalSettings.data.footer as FooterType;
+    const contactInfoGlobal = globalSettings.data.contactInfo as ContactInfoType;
+    const socialMedia = globalSettings.data.socialMedia as SocialMediaItemType[];
+    header = globalSettings.data.header as HeaderTypeData;
+    footerWithContactInfo = {
+      ...footer,
+      contactInfoGlobal,
+      socialMedia,
+    };
+  } catch (err) {
+    console.error("Error loading global settings (Strapi):", err);
+    header = {} as HeaderTypeData;
+    footerWithContactInfo = {} as FooterType & { contactInfoGlobal: ContactInfoType; socialMedia: SocialMediaItemType[] };
+  }
 
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body 
         className={`${interTight.variable} antialiased bg-background-1`}
         suppressHydrationWarning={true}
       >
-        {gtmId && (
-          <>
-            <Script
-              id="gtm"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','${gtmId}');
-                `,
-              }}
-            />
-            <noscript>
-              <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-                height="0"
-                width="0"
-                style={{ display: "none", visibility: "hidden" }}
-                title="Google Tag Manager"
-              />
-            </noscript>
-          </>
-        )}
+        <GoogleTagManager />
         <AOSProvider>
           <ClientLayoutWrapper header={header} footer={footerWithContactInfo}>
             {/* --- SCRIPTS DE GOOGLE TRANSLATE --- */}
