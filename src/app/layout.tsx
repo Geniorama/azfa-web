3203fe6b5233c08c35ab0ea38d8bb6499cf9c242
angelpublicista <angelpublicista.1@gmail.com>
2026-03-04 10:11:24 -0500

    feat: GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,12 +52,40 @@ export default async function RootLayout({
 
   console.log("footerWithContactInfo", footerWithContactInfo);
 
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <html lang="es">
       <body 
         className={`${interTight.variable} antialiased bg-background-1`}
         suppressHydrationWarning={true}
       >
+        {gtmId && (
+          <>
+            <Script
+              id="gtm"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${gtmId}');
+                `,
+              }}
+            />
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+                title="Google Tag Manager"
+              />
+            </noscript>
+          </>
+        )}
         <AOSProvider>
           <ClientLayoutWrapper header={header} footer={footerWithContactInfo}>
             {/* --- SCRIPTS DE GOOGLE TRANSLATE --- */}
